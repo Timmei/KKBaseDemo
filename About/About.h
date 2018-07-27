0168d9d49fa4cbddd92702df8a49d64e0c5fee6d
Tim Mei <tim.mei@btcc.com>
2018-07-27 15:31:30 +0800

    add RN

---
+++ About/About.h
@@ -0,0 +1,16 @@
+//
+//  About.h
+//  About
+//
+//  Created by Tim Mei on 2018/7/26.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface About : NSObject
+
++ (UIViewController *)aboutViewController;
+
+@end
