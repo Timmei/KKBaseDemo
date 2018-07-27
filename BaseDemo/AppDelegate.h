a583204653f90c193045b39fa1795bea5a218057
Tim Mei <tim.mei@btcc.com>
2018-07-27 14:11:34 +0800

    first commit

---
+++ BaseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseDemo
+//
+//  Created by Tim Mei on 2018/7/26.
+//  Copyright © 2018 Tim Mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BaseDemo
+//
+//  Created by Tim Mei on 2018/7/26.
+//  Copyright © 2018 Tim Mei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
